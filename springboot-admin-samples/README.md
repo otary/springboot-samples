977f585b923bb6567d1f0f3a0f130126b13a9671
chenzw <656469722@qq.com>
2019-06-13 20:37:27 +0800

    initialization

---
+++ springboot-admin-samples/README.md
@@ -0,0 +1,14 @@
+# springboot-admin-samples
+
+## springboot-admin-server-samples
+
+admin服务端
+
+http://localhost:8000
+
+
+## springboot-admin-client-samples
+
+admin客户端（可多个）
+
+- 需要指定服务端地址（spring.boot.admin.client.url）
---
+++ springboot-admin-samples/springboot-admin-client-samples/src/main/java/cn/chenzw/springboot/admin/client/AdminClientSamplesApp.java
@@ -0,0 +1,12 @@
+package cn.chenzw.springboot.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminClientSamplesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientSamplesApp.class, args);
+    }
+}
