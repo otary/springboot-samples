4f6feb43145644050e1555df6244cf208275608c
chenzw <656469722@qq.com>
2019-06-14 13:02:23 +0800

    initialization

---
+++ springboot-datasources-samples/springboot-datasources-h2-samples/src/main/java/cn/chenzw/springboot/datasources/h2/H2SamplesApp.java
@@ -0,0 +1,12 @@
+package cn.chenzw.springboot.datasources.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2SamplesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2SamplesApp.class, args);
+    }
+}
