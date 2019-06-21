c8effdbc5a750a2ec2a8e7f6cce1104cc7b12740
chenzw <chenzw@ffcs.cn>
2019-06-21 10:30:08 +0800

    initialization

--- springboot-swagger-samples/src/main/java/cn/chenzw/springboot/swagger/controllers/SwaggerController.java
+++ springboot-swagger-samples/src/main/java/cn/chenzw/springboot/swagger/controllers/SwaggerController.java
@@ -2,21 +2,18 @@ package cn.chenzw.springboot.swagger.controllers;
 
 import cn.chenzw.springboot.swagger.domain.entify.SwaggerEntity;
 import cn.chenzw.springboot.swagger.domain.entify.SwaggerParamEntity;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @Api(value = "@Api-value", tags = {"@Api-tags1", "@Api-tags2"}, protocols = "https")
 @RestController
 @RequestMapping("/swagger-test")
 public class SwaggerController {
 
-    @ApiOperation(value = "@ApiOperation-value", notes = "@ApiOperation-notes", tags = {"@ApiOperation-tags1", "@ApiOperation-tags2"},
-            response = SwaggerEntity.class, nickname = "@ApiOperation-nickname")
+    @ApiOperation(value = "@ApiOperation-value", notes = "@ApiOperation-notes", tags = {"@ApiOperation-tags1",
+            "@ApiOperation-tags2"}, response = SwaggerEntity.class, nickname = "@ApiOperation-nickname")
     @GetMapping("/basic")
     public SwaggerEntity basic() {
         return new SwaggerEntity();
@@ -25,16 +22,22 @@ public class SwaggerController {
 
     @ApiOperation(value = "带参数的GET请求", notes = "带参数的GET请求notes")
     @ApiImplicitParams({
-            @ApiImplicitParam(name = "sex", value = "@ApiImplictParam-value", defaultValue = "@ApiImplictParam-defaultValue",
-                    allowableValues = "男,女", required = true
-            ),
-            @ApiImplicitParam(name = "query", value = "@ApiImplictParam-value", defaultValue = "@ApiImplictParam-defaultValue",
-            access = "test")
-    })
+            @ApiImplicitParam(name = "sex", value = "@ApiImplictParam-value", defaultValue = "@ApiImplictParam-defaultValue", allowableValues = "男,女", required = true, example = "女"),
+            @ApiImplicitParam(name = "query", value = "@ApiImplictParam-value", defaultValue = "@ApiImplictParam-defaultValue", access = "test", example = "example")})
     @GetMapping("/get-with-args")
     public SwaggerEntity testGetWithArgs(SwaggerParamEntity swaggerParamEntity, String sex, String query) {
         return new SwaggerEntity();
     }
+
+    @ApiOperation(value = "Map参数测试", notes = "Map请求参数和响应值测试")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "map2", paramType = "body", examples = @Example({
+            @ExampleProperty(value = "{'user':'id'}", mediaType = "application/json")}))})
+    @PostMapping("/post-map-args")
+    public Map<String, String> testMapArgs(@RequestBody Map<String, String> map2) {
+        return map2;
+    }
+
 
     // post测试
     // @TODO
