cd2345f4b9fbb1915c759abb8c59c604ba5a5f33
chenzw <chenzw@ffcs.cn>
2019-06-21 18:13:39 +0800

    initialization

---
+++ springboot-datasources-samples/springboot-datasource-multiple-samples/springboot-datasource-multiple-annotation-samples/src/main/java/cn/chenzw/springboot/datasource/multiple/annotation/aop/DataSourceAspect.java
@@ -0,0 +1,48 @@
+package cn.chenzw.springboot.datasource.multiple.annotation.aop;
+
+import cn.chenzw.springboot.datasource.multiple.annotation.support.mybatis.DataSource;
+import cn.chenzw.springboot.datasource.multiple.annotation.support.mybatis.DataSourceHolder;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+/**
+ * 数据源切面
+ */
+@Aspect
+@Component
+@Order(-99)
+public class DataSourceAspect {
+
+
+    private static final Logger logger = LoggerFactory.getLogger(DataSourceAspect.class);
+    private static final String POINT_CUT = "datasource()";
+
+
+    @Pointcut("@annotation(cn.chenzw.springboot.datasource.multiple.annotation.support.mybatis.DataSource)")
+    public void datasource() {
+
+    }
+
+    /**
+     * 拦截DataSource注解,并注入数据源
+     */
+    @Before(POINT_CUT)
+    public void before(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        DataSource dataSource = method.getAnnotation(DataSource.class);
+        // @TODO 为空判断？
+        DataSourceHolder.set(dataSource.value());
+    }
+
+
+}
---
+++ springboot-datasources-samples/springboot-datasource-multiple-samples/springboot-datasource-multiple-annotation-samples/src/main/java/cn/chenzw/springboot/datasource/multiple/annotation/support/mybatis/DataSourceHolder.java
@@ -0,0 +1,31 @@
+package cn.chenzw.springboot.datasource.multiple.annotation.support.mybatis;
+
+/**
+ * 线程级数据源持有者
+ */
+public abstract class DataSourceHolder {
+
+    private static final ThreadLocal<String> dsTL = new ThreadLocal<>();
+
+    /**
+     * 设置数据源
+     */
+    public static void set(String dataSource) {
+        dsTL.set(dataSource);
+    }
+
+    /**
+     * 获取数据源
+     * @return
+     */
+    public static String get() {
+        return dsTL.get();
+    }
+
+    /**
+     * 清除数据源
+     */
+    public static void clear() {
+        dsTL.remove();
+    }
+}
