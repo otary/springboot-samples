a18b584f3cc5c1e08f743a92c15d4b44ccbc24a9
chenzw <chenzw@ffcs.cn>
2019-08-19 17:34:53 +0800

    initialization

---
+++ springboot-datasources-samples/springboot-datasources-mysql-samples/src/main/java/cn.chenzw.springboot.datasources.mysql/MysqlSamplesApp.java
@@ -0,0 +1,12 @@
+package cn.chenzw.springboot.datasources.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlSamplesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlSamplesApp.class, args);
+    }
+}
---
+++ springboot-datasources-samples/springboot-datasources-oracle-samples/src/main/java/cn/chenzw/springboot/datasources/oracle/OracleSamplesApp.java
@@ -0,0 +1,12 @@
+package cn.chenzw.springboot.datasources.oracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleSamplesApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleSamplesApp.class, args);
+    }
+}
