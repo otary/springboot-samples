41b9fda564d79e7fb301ef481862154376b62b24
chenzw <656469722@qq.com>
2019-09-19 20:01:24 +0800

    initialization

--- springboot-config-samples/springboot-configproperties-samples/src/main/java/cn/chenzw/springboot/config/properties/SSOProperties.java
+++ springboot-config-samples/springboot-configproperties-samples/src/main/java/cn/chenzw/springboot/config/properties/SSOProperties.java
@@ -5,6 +5,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import java.util.List;
+
 /**
  * 普通字段注入示例
  */
@@ -22,6 +24,18 @@ public class SSOProperties {
      */
     @Value("${sso.default-value:默认值}")
     private String defaultValue;
+
+    /**
+     * 集合注入
+     */
+    @Value("${sso.ts[0]}")
+    private List<String> ts;
+
+    /**
+     * 集合注入（默认值）
+     */
+    @Value("${sso.defalut-ts[0]:aaa,bbb}")
+    private List<String> defaultTs;
 
     public String getPrivateKey() {
         return privateKey;
@@ -47,12 +61,30 @@ public class SSOProperties {
         this.defaultValue = defaultValue;
     }
 
+    public List<String> getTs() {
+        return ts;
+    }
+
+    public void setTs(List<String> ts) {
+        this.ts = ts;
+    }
+
+    public List<String> getDefaultTs() {
+        return defaultTs;
+    }
+
+    public void setDefaultTs(List<String> defaultTs) {
+        this.defaultTs = defaultTs;
+    }
+
     @Override
     public String toString() {
         return "SSOProperties{" +
                 "privateKey='" + privateKey + '\'' +
                 ", defaultUrl='" + defaultUrl + '\'' +
                 ", defaultValue='" + defaultValue + '\'' +
+                ", ts=" + ts +
+                ", defaultTs=" + defaultTs +
                 '}';
     }
 }
