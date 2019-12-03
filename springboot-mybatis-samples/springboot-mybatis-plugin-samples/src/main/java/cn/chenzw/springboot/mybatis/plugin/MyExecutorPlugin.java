3c0419fce0f20a3c0c5c183fda26b3458b17a4ff
chenzw <656469722@qq.com>
2019-12-03 22:34:24 +0800

    initialization

---
+++ springboot-mybatis-samples/springboot-mybatis-plugin-samples/src/main/java/cn/chenzw/springboot/mybatis/plugin/MyExecutorPlugin.java
@@ -0,0 +1,72 @@
+package cn.chenzw.springboot.mybatis.plugin;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.plugin.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.util.Properties;
+
+/**
+ *
+ */
+@Intercepts({@Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class})})
+public class MyExecutorPlugin implements Interceptor {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        logger.info("[intercept]:{}", invocation);
+
+        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+
+        // 获取 SQL 命令
+        SqlCommandType sqlCommandType = mappedStatement.getSqlCommandType();
+
+        // 获取参数
+        Object parameter = invocation.getArgs()[1];
+
+        if (parameter != null) {
+            // 获取成员变量
+            Field[] declaredFields = parameter.getClass().getDeclaredFields();
+
+           /* for (Field field : declaredFields) {
+                if (field.getAnnotation(CreateTime.class) != null) {
+                    if (SqlCommandType.INSERT.equals(sqlCommandType)) { // insert 语句插入 createTime
+                        field.setAccessible(true);
+                        if (field.get(parameter) == null) {
+                            field.set(parameter, new Date());
+                        }
+                    }
+                }
+
+                if (field.getAnnotation(UpdateTime.class) != null) { // insert 或 update 语句插入 updateTime
+                    if (SqlCommandType.INSERT.equals(sqlCommandType) || SqlCommandType.UPDATE.equals(sqlCommandType)) {
+                        field.setAccessible(true);
+                        if (field.get(parameter) == null) {
+                            field.set(parameter, new Date());
+                        }
+                    }
+                }
+            }*/
+        }
+
+
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        logger.info("[plugin]: {}", target);
+        return Plugin.wrap(target, this);
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+        logger.info("[setProperties]:{}", properties);
+    }
+}
