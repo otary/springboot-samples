8391fe2d60700f3f6600012cf7bc9245ba71a9aa
chenzw <656469722@qq.com>
2020-01-05 20:40:50 +0800

    initialization

--- springboot-batch-samples/springboot-batch-basic-samples/src/main/java/cn/chenzw/springboot/batch/basic/samples/config/BatchAdminConfig.java
+++ springboot-batch-samples/springboot-batch-basic-samples/src/main/java/cn/chenzw/springboot/batch/basic/samples/config/BatchAdminConfig.java
@@ -1,6 +1,6 @@
 package cn.chenzw.springboot.batch.basic.samples.config;
 
-import org.springframework.batch.admin.annotation.EnableBatchAdmin;
+//import org.springframework.batch.admin.annotation.EnableBatchAdmin;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
